75ad90aa74f14e9f78b837f8d3b344c28254f2ac
owen7932 <opmcmenaman@crimson.ua.edu>
2024-10-07 17:53:54 -0500

    Final commit

--- obj/Debug/net8.0/Fall2024-Assignment2-opmcmenaman.AssemblyInfo.cs
+++ obj/Debug/net8.0/Fall2024-Assignment2-opmcmenaman.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Fall2024-Assignment2-opmcmenaman")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94623f775ac954090c3f46e768d449db39665de7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Fall2024-Assignment2-opmcmenaman")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Fall2024-Assignment2-opmcmenaman")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- wwwroot/script.js
+++ wwwroot/script.js
@@ -7,10 +7,10 @@ function apiSearch() {
     };
 
     $.ajax({
-        url: 'https://MY-API-ENDPOINT/v7.0/search?' + $.param(params),
+        url: 'https://api.bing.microsoft.com//v7.0/search?' + $.param(params),
         type: 'GET',
         headers: {
-            'Ocp-Apim-Subscription-Key': 'MY-API-KEY'
+            'Ocp-Apim-Subscription-Key': 'c67bfb18c1104e498e9d5b60baf39712'
         }
     })
         .done(function (data) {
@@ -27,3 +27,96 @@ function apiSearch() {
             alert('error');
         });
 }
+
+//$("#searchButton").click(function () {
+//    doSearch();
+//});
+$("#searchButton").click(function () {
+    apiSearch();
+})
+
+//var textShowing = true;
+
+//$("#toggle").click(function () {
+//    if (textShowing) {
+//        $("#text").fadeOut(function () {
+//            textShowing = false;
+//        });
+//    } else {
+//        $("#text").fadeIn(function () {
+//            textShowing = true;
+//        });
+//    }
+
+var imageIndex = 1;
+
+$("#Header").click(function () {
+    var currIndex = imageIndex;
+    while(imageIndex == currIndex){
+        imageIndex = Math.floor(Math.random() * 10) + 1;
+    }
+    $("body").css("background-image", "url('./Background" + imageIndex + ".jpg')");
+
+    setTimeout(function() {
+        var currIndex = imageIndex;
+        while(imageIndex == currIndex){
+            imageIndex = Math.floor(Math.random() * 10) + 1;
+        }
+        $("body").css("background-image", "url('./Background" + imageIndex + ".jpg')");
+    }, 500);
+})
+
+function getTime(){
+    var t = new Date();
+    var hours = t.getHours();
+    var minutes = t.getMinutes();
+    if(minutes < 10){
+        minutes = "0" + minutes;
+    }
+    if(hours < 10){
+        hours = "0" + hours;
+    }
+
+    return hours + ":" + minutes;
+}
+
+$("#timeButton").click(function() {
+    var currTime = getTime();
+    $("#Time").html(currTime);
+    $("#Time").dialog();
+})
+
+$("#luckyButton").click(function(){
+    feelingLucky();
+})
+
+function feelingLucky() {
+    var params = {
+        'q': $('#query').val(),
+        'count': 50,
+        'offset': 0,
+        'mkt': 'en-us'
+    };
+
+    $.ajax({
+        url: 'https://api.bing.microsoft.com//v7.0/search?' + $.param(params),
+        type: 'GET',
+        headers: {
+            'Ocp-Apim-Subscription-Key': 'c67bfb18c1104e498e9d5b60baf39712'
+        }
+    })
+        .done(function (data) {
+            var len = data.webPages.value.length;
+            if(len > 0){
+                var first = data.webPages.value[0].url;
+                window.location.href = first;
+            }
+            else alert("No Results");
+        })
+        .fail(function () {
+            alert('error');
+        });
+}
+
+
+
